bc002b99b66fd132fbc728141a7fc6b370b69b2f
ajilk <ajilkhomov@gmail.com>
2019-09-20 21:42:02 -0400

    Retrieve info from database

--- zip-api/app.js
+++ zip-api/app.js
@@ -1,28 +1,20 @@
 const express = require('express');
 const app = express();
-
 const zipdb = require('./zipData');
 
 const PORT = process.env.PORT || 8000;
 
-
-// console.log(zipdb.byCity);
-
-
 app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+  res.json({ test: 'Yay' });
 });
-
 
 app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+  res.send(zipdb.byZip[req.params.zipcode])
 });
-
 
 app.get('/city/:cityname', (req, res) => {
-  // fill in...
+  res.send(zipdb.byCity[req.params.cityname.toUpperCase()])
 });
-
 
 app.listen(PORT, () => {
   console.log(`zip-api is up and running on ${PORT}`);
